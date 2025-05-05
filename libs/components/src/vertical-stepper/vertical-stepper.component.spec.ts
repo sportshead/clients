96823681237123bc8f2ec8fc2e4c59b576610b39
Bryan Cunningham <bryan.cunningham@me.com>
2025-05-05 15:27:18 -0400

    WIP

---
+++ libs/components/src/vertical-stepper/vertical-stepper.component.spec.ts
@@ -0,0 +1,49 @@
+// FIXME: Update this file to be type safe and remove this and next line
+// @ts-strict-ignore
+import { Component, DebugElement } from "@angular/core";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+
+import { VerticalStepperModule } from "./index";
+
+describe("VerticalStepper", () => {
+  let fixture: ComponentFixture<TestApp>;
+  let testAppComponent: TestApp;
+  // let buttonDebugElement: DebugElement;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [VerticalStepperModule],
+      declarations: [TestApp],
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    TestBed.compileComponents();
+    fixture = TestBed.createComponent(TestApp);
+    testAppComponent = fixture.debugElement.componentInstance;
+    // buttonDebugElement = fixture.debugElement.query(By.css("button"));
+    fixture.detectChanges();
+    // disabledButtonDebugElement = fixture.debugElement.query(By.css("button#disabled"));
+    // linkDebugElement = fixture.debugElement.query(By.css("a"));
+  }));
+
+  it("should be defined", () => {
+    expect(testAppComponent).toBeDefined();
+  });
+});
+
+@Component({
+  selector: "test-app",
+  template: `
+    <bit-vertical-stepper>
+      <bit-vertical-step label="Step 1">
+        <p>Step 1 content</p>
+      </bit-vertical-step>
+      <bit-vertical-step label="Step 2">
+        <p>Step 2 content</p>
+      </bit-vertical-step>
+    </bit-vertical-stepper>
+  `,
+})
+class TestApp {
+}
