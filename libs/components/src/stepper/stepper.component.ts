a105b02c670da41cf41dabf68db38ff896a8aa5c
Bryan Cunningham <bryan.cunningham@me.com>
2025-05-08 11:53:57 -0700

    add basic responsizeness to stepper

--- libs/components/src/stepper/stepper.component.ts
+++ libs/components/src/stepper/stepper.component.ts
@@ -2,16 +2,24 @@ import { CdkStepper } from "@angular/cdk/stepper";
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 
+import { ResizeObserverDirective } from "../resize-observer/resize-observer.directive";
+
 import { StepContentComponent } from "./step-content.component";
 
 @Component({
   selector: "bit-stepper",
   templateUrl: "stepper.component.html",
   providers: [{ provide: CdkStepper, useExisting: StepperComponent }],
-  imports: [CommonModule, StepContentComponent],
+  imports: [CommonModule, ResizeObserverDirective, StepContentComponent],
   standalone: true,
 })
 export class StepperComponent extends CdkStepper {
+  resizeWidthsMap = new Map([
+    [2, 600],
+    [3, 768],
+    [4, 900]
+  ]);
+
   isStepDisabled(index: number) {
     if (this.selectedIndex !== index) {
       return this.selectedIndex === index - 1
@@ -23,6 +31,18 @@ export class StepperComponent extends CdkStepper {
 
   selectStepByIndex(index: number): void {
     this.selectedIndex = index;
+  }
+
+  private initialOrientation = this.orientation;
+
+  handleResize(entry: ResizeObserverEntry) {
+    const width = entry.contentRect.width;
+    const numberOfSteps = this.steps.length;
+
+    if (this.initialOrientation === "horizontal") {
+      this.orientation = width < this.resizeWidthsMap.get(numberOfSteps) ? "vertical": "horizontal";
+      this._stateChanged();
+    }
   }
 
   /**
